d2d79f325ce83cd89b6e0cf500e92ad2860216de
Jasmine-mai <>
2025-02-09 14:32:46 +0700

    XpathHomeWork

--- AutomationSelenium/src/main/java/automation/testsuite/LoginTest.java
+++ AutomationSelenium/src/main/java/automation/testsuite/LoginTest.java
@@ -1,13 +1,63 @@
 package automation.testsuite;
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+import org.openqa.selenium.WebElement;
+import automation.common.CommonBase;
 
-public class LoginTest {
+public class LoginTest extends CommonBase {
+	@BeforeMethod
+	public void openBrowserTest()
+	{
+		driver = initBrowser("https://alada.vn/tai-khoan/dang-nhap.html");
+	}
 	
 	@Test
 	
-	public void hello()
+	public void locateById()
 	{
-		System.out.println("Hello my automation class December");
+		WebElement textUsername = driver.findElement(By.id("txtLoginUsername"));
+		System.out.println("Username element is: " +textUsername);
+		
+		WebElement textPassword = driver.findElement(By.id("txtLoginPassword"));
+		System.out.println("Password element is: " +textPassword);
 	}
-
+	
+	@Test
+	public void locateByName()
+	{
+		WebElement textUsername = driver.findElement(By.name("txtLoginUsername"));
+		System.out.println("Username element is: " +textUsername);
+		
+		WebElement textPassword = driver.findElement(By.name("txtLoginPassword"));
+		System.out.println("Password element is: " +textPassword);
+	}
+	
+	@Test
+	public void locateByClassName()
+	{
+		WebElement btnFaceBook = driver.findElement(By.className("btn_face"));
+		System.out.println("Button facebook element is: " +btnFaceBook);
+		
+	}
+	
+	@Test
+	public void locateByLinkText()
+	{
+		WebElement linkForgotPassword = driver.findElement(By.linkText("Quên mật khẩu?"));
+		System.out.println("Link Forgot Password is: " +linkForgotPassword);	
+		
+		WebElement linkGiangVien = driver.findElement(By.partialLinkText("giảng viên"));
+		System.out.println("Link login for trainer is: " +linkGiangVien);	
+		
+	}
+	
+	@Test
+	public void locateByTagName()
+	{
+		WebElement textInput = driver.findElement(By.tagName("input"));
+		System.out.println(textInput);	
+	}
+	
+	
+	
 }
